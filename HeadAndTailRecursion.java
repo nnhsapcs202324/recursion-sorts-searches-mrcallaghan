3f9910154c4276961af426e31fdead4b1cdc689a
mrcallaghan <mcallaghan@naperville203.org>
2024-03-22 15:05:22 -0500

    finished isPalindrome

--- HeadAndTailRecursion.java
+++ HeadAndTailRecursion.java
@@ -18,17 +18,21 @@ public class HeadAndTailRecursion
             return;
 
         System.out.print(str.charAt(0));
-
+        
+        // tail recursion means the recursive call is at the end of the method
         tail(str.substring(1));
     }
 
     public static void head(String str)
     {
+        // terminating condition (empty loaf)
         if(str.length() == 0)
             return;
         
+        // recursive call comes before the end of the method in head recursion
         head(str.substring(1));
-
+        
+        // print one character (contribute one small part to the larger solution)
         System.out.print(str.charAt(0));
     }
 }
